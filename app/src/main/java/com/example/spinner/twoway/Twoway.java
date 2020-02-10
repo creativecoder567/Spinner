0767bfeccf0a8596a31ff3700fd97a98265c4c7a
creativecoder567 <sarathcreativethinker@gmail.com>
2020-02-10 15:18:48 +0530

    ->twowaybinding classes are imported. ->layout problem is fixed.

---
+++ app/src/main/java/com/example/spinner/twoway/Twoway.java
@@ -0,0 +1,16 @@
+package com.example.spinner.twoway;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.spinner.R;
+
+public class Twoway extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_twoway);
+    }
+}
