6df76f43e037aeeb3d573b8ccd4e5b6f607f021c
creativecoder567 <sarathcreativethinker@gmail.com>
2020-02-10 15:59:42 +0530

    ->Twowaybinding is done using Baseobservable class.

--- app/src/main/java/com/example/spinner/twoway/Twoway.java
+++ app/src/main/java/com/example/spinner/twoway/Twoway.java
@@ -1,16 +1,110 @@
 package com.example.spinner.twoway;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
-import com.example.spinner.R;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
 
-public class Twoway extends AppCompatActivity {
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.example.spinner.R;
+import com.example.spinner.databinding.ActivityTwowayBinding;
+
+import java.util.UUID;
+
+
+public class Twoway extends AppCompatActivity implements SettingsEventListener{
+    private static final String TAG = "MainActivity";
+//    private FirebaseFirestore firestoreDB;
+    private String uniqueIdentifier;
+    private ActivityTwowayBinding binding;
+    private AccountSetting accountSetting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_twoway);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_twoway);
+        binding.setEventListener(this);
+        String[] paymentOptions = getResources().getStringArray(R.array.payment_options);
+        binding.setSpinAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_dropdown_item, paymentOptions));
+        accountSetting = new AccountSetting();
+        binding.setAccountSettings(accountSetting);
+
+//        firestoreDB = FirebaseFirestore.getInstance();
+//        loadSettings();
+    }
+    /*private void loadSettings(){
+        firestoreDB.collection("accountConfig").document(getInstallationIdentifier())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            if(task.getResult().exists()) {
+                                accountSetting =
+                                        task.getResult().toObject(AccountSetting.class);
+                                if(accountSetting.getCommunicationMode() == null){
+                                    accountSetting.setCommunicationMode("");
+                                }
+                            }else {
+                                accountSetting = new AccountSetting();
+                            }
+                            //bind data
+                            binding.setAccountSettings(accountSetting);
+                        }else {
+                            Log.d(TAG, "Error loading settings", task.getException());
+                        }
+                    }
+                });
+
+    }*/
+    public void onClickSaveSettings(View v) {
+        accountSetting.setAccountName(accountSetting.getAccountName().toUpperCase());
+        Log.d(TAG, "onCreate: "+accountSetting.getAccountName());
+        Log.d(TAG, "onCreate: "+accountSetting.getCommunicationMode());
+        Log.d(TAG, "onCreate: "+accountSetting.getDefaultPaymentOption());
+//        saveSettings(accountSetting);
+    }
+    public void onCommunicationMode(String commMode){
+        accountSetting.setCommunicationMode(commMode);
+    }
+    /*private void saveSettings(AccountSetting accountSetting){
+        firestoreDB.collection("accountConfig").document(getInstallationIdentifier())
+                .set(accountSetting)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(MainActivity.this,
+                                    "settings have been saved",
+                                    Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(MainActivity.this,
+                                    "settings could not be saved",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }*/
+    public synchronized String getInstallationIdentifier() {
+        if (uniqueIdentifier == null) {
+            SharedPreferences sharedPrefs = this.getSharedPreferences(
+                    "UNIQUE_ID", Context.MODE_PRIVATE);
+            uniqueIdentifier = sharedPrefs.getString("UNIQUE_ID", null);
+            if (uniqueIdentifier == null) {
+                uniqueIdentifier = UUID.randomUUID().toString();
+                SharedPreferences.Editor editor = sharedPrefs.edit();
+                editor.putString("UNIQUE_ID", uniqueIdentifier);
+                editor.commit();
+            }
+        }
+        return uniqueIdentifier;
     }
 }
